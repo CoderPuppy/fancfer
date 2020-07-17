7c2f0e1e13f480e195e91146e37b5acb12833236
CoderPuppy <coderpup@gmail.com>
2020-07-17 11:52:04 -0400

    More work on Swift implementation

--- swift/Sources/Fancfer/main.swift
+++ swift/Sources/Fancfer/main.swift
@@ -1,21 +1,52 @@
 import Crypto
-// import Glibc
+import Glibc
 import FancferC
+import Foundation
+
+extension Sequence {
+	@inlinable
+	func withUnsafeBufferPointer<R>(
+		_ body: (UnsafeBufferPointer<Element>) throws -> R
+	) rethrows -> R {
+		if let ret = try withContiguousStorageIfAvailable(body) {
+			return ret
+		} else {
+			return try ContiguousArray(self).withUnsafeBufferPointer(body)
+		}
+	}
+}
+
+extension HashFunction {
+	@inlinable
+	mutating func update<S: Sequence>(sequence: S) {
+		sequence.withUnsafeBufferPointer { (ptr) in
+			self.update(bufferPointer: UnsafeRawBufferPointer(ptr))
+		}
+	}
+}
 
 struct CError: Error {
 	let errno: CInt
+
+	init(errno: CInt) {
+		self.errno = errno
+	}
+
+	init() {
+		self.init(errno: Glibc.errno)
+	}
 }
 
 struct FSRef {
 	let dir: Directory?
 	let path: String
 
-	func _open(flags: CInt, mode: CInt) throws -> CInt {
-		let fd = path.utf8CString.withUnsafeBufferPointer { (ptr) in
-			openat(dir?.fd ?? AT_FDCWD, ptr.baseAddress!, flags, mode)
+	func _open(flags: CInt, mode: Glibc.mode_t) throws -> CInt {
+		let fd = path.withCString { (ptr) in
+			Glibc.openat(dir?.fd ?? AT_FDCWD, ptr, flags, mode)
 		}
 		guard fd != -1 else {
-			throw CError(errno: errno)
+			throw CError(errno: Glibc.errno)
 		}
 		return fd
 	}
@@ -25,8 +56,8 @@ struct FSRef {
 	}
 }
 
-let normalFileMode = S_IROTH | S_IRGRP | S_IRUSR | S_IWUSR
-let normalDirMode = S_IXOTH | S_IROTH | S_IXGRP | S_IRGRP | S_IXUSR | S_IRUSR | S_IWUSR
+let normalFileMode = Glibc.S_IROTH | Glibc.S_IRGRP | Glibc.S_IRUSR | Glibc.S_IWUSR
+let normalDirMode = Glibc.S_IXOTH | Glibc.S_IROTH | Glibc.S_IXGRP | Glibc.S_IRGRP | Glibc.S_IXUSR | Glibc.S_IRUSR | Glibc.S_IWUSR
 
 class Directory {
 	var ptr: OpaquePointer
@@ -36,8 +67,8 @@ class Directory {
 	}
 
 	convenience init(ref: FSRef) throws {
-		let fd = try ref._open(flags: O_DIRECTORY, mode: 0)
-		let ptr = fdopendir(fd)!
+		let fd = try ref._open(flags: Glibc.O_DIRECTORY, mode: 0)
+		let ptr = Glibc.fdopendir(fd)!
 		self.init(ptr: ptr)
 	}
 
@@ -47,62 +78,300 @@ class Directory {
 		FSRef(dir: dir, path: path)
 	}
 
-	func makeTempFile(mode: CInt = normalFileMode) throws -> File {
-		let fd = openat(dirfd(ptr), ".", O_TMPFILE | O_RDWR, normalFileMode)
+	func makeTempFile(mode: Glibc.mode_t = normalFileMode) throws -> File {
+		let fd = Glibc.openat(dirfd(ptr), ".", O_TMPFILE | O_RDWR, mode)
 		guard fd != -1 else {
-			throw CError(errno: errno)
+			throw CError(errno: Glibc.errno)
 		}
-		let ptr = fdopen(fd, "w+")!
+		let ptr = Glibc.fdopen(fd, "w+")!
 		return File(ptr: ptr)
 	}
 }
 
-class File {
-	var ptr: OpaquePointer
+class File: TextOutputStream {
+	var ptr: UnsafeMutablePointer<Glibc.FILE>
 
-	init(ptr: OpaquePointer) {
+	init(ptr: UnsafeMutablePointer<Glibc.FILE>) {
 		self.ptr = ptr
 	}
 
+	deinit {
+		fclose(self.ptr)
+	}
+
 	var fd: CInt { get { fileno(ptr) } }
+
+	func write(_ ptr: UnsafeBufferPointer<UInt8>) throws {
+		guard Glibc.fwrite(ptr.baseAddress!, ptr.count, 1, self.ptr) != -1 else {
+			throw CError(errno: Glibc.errno)
+		}
+	}
+
+	func write(_ string: String) {
+		do {
+			try string.utf8.withUnsafeBufferPointer { (ptr) in
+				try self.write(ptr)
+			}
+		} catch {
+			// TODO: TextOutputStream does not allow this to throw
+		}
+	}
+
+	func currentPos() throws -> Int {
+		let pos = Glibc.ftell(self.ptr)
+		guard pos != -1 else {
+			throw CError(errno: Glibc.errno)
+		}
+		return pos
+	}
+
+	func seekTo(startPlus count: Int) throws {
+		guard Glibc.fseek(self.ptr, count, Glibc.SEEK_SET) != -1 else {
+			throw CError(errno: Glibc.errno)
+		}
+	}
+
+	func readAll(initialSize: Int = 1024) throws -> UnsafeMutableBufferPointer<Int8> {
+		var buffer = Glibc.malloc(initialSize)!
+		var nread = 0
+		var remaining = initialSize
+		while Glibc.feof(self.ptr) == 0 {
+			if remaining == 0 {
+				buffer = Glibc.realloc(buffer, nread * 2)!
+				remaining += nread
+			}
+			let len = Glibc.fread(buffer + nread, 1, remaining, self.ptr)
+			guard Glibc.ferror(self.ptr) == 0 else {
+				// TODO: I'm not sure about this
+				throw CError(errno: Glibc.errno)
+			}
+			nread += len
+			remaining -= len
+		}
+		return UnsafeMutableRawBufferPointer(start: buffer, count: nread).bindMemory(to: Int8.self)
+	}
+}
+
+extension File {
+	class Getdelim {
+		var line: UnsafeMutablePointer<Int8>? = nil
+		var lineCap: Int = 0
+
+		deinit {
+			if line != nil {
+				Glibc.free(line)
+			}
+		}
+
+		func read(from file: File, delimitedBy delimiter: CChar) throws -> String? {
+			Glibc.errno = 0
+			let len = withUnsafeMutablePointer(to: &self.line) { (line_ptr) in
+				Glibc.getdelim(line_ptr, &self.lineCap, Int32(delimiter), file.ptr)
+			}
+			guard len != -1 else {
+				let err = Glibc.errno
+				// TODO: this is terrible, but I don't know a better way
+				if err == 0 {
+					return nil
+				} else {
+					throw CError(errno: err)
+				}
+			}
+			return String(cString: line!)
+		}
+	}
+}
+
+extension Digest {
+	func hexString() -> String {
+		return Array(self).map { String(format: "%02x", $0) }.joined()
+	}
+}
+
+struct InvalidObject: Error {
 }
 
 struct Store<H: HashFunction> {
 	var dir: Directory
 	var hf: H.Type
 
-	func ref(_ hash: H.Digest) -> FSRef {
-		FSRef(dir: dir, path: Array(hash).map { String(format: "%02x", $0) }.joined())
+	struct Object {
+		let hash: String
+		let type: String
+		var file: File
+
+		internal init(hash: String, file: File) throws {
+			self.hash = hash
+			self.file = file
+			self.type = try "user.fancfer.object-type".withCString { (key_ptr) in
+				let expectedSize = FancferC.fgetxattr(file.fd, key_ptr, nil, 0)
+				guard expectedSize != -1 else {
+					throw CError(errno: Glibc.errno)
+				}
+				return try String(unsafeUninitializedCapacity: Int(expectedSize), initializingUTF8With: { (buffer) in
+					let realSize = FancferC.fgetxattr(file.fd, key_ptr, UnsafeMutableRawPointer(buffer.baseAddress!), buffer.count)
+					guard realSize != -1 else {
+						throw CError(errno: Glibc.errno)
+					}
+					return Int(realSize)
+				})
+			}
+		}
+
+		func contents() throws -> String {
+			try self.file.seekTo(startPlus: 0)
+			let buffer = try self.file.readAll()
+			defer { Glibc.free(buffer.baseAddress!) }
+			return String(cString: buffer.baseAddress!)
+		}
+
+		func dir() throws -> Dictionary<String, String> {
+			guard self.type == "dir" else { throw InvalidObject() }
+			try self.file.seekTo(startPlus: 0)
+			let getdelim = File.Getdelim()
+			var dict = Dictionary<String, String>()
+			while let hash = try getdelim.read(from: self.file, delimitedBy: 0) {
+				guard let name = try getdelim.read(from: self.file, delimitedBy: 0) else {
+					throw InvalidObject()
+				}
+				dict[name] = hash
+			}
+			return dict
+		}
+
+		func blob() throws -> String {
+			guard self.type == "blob" else { throw InvalidObject() }
+			return try self.contents()
+		}
+
+		func src() throws -> (String, String) {
+			guard self.type == "src" else { throw InvalidObject() }
+			try self.file.seekTo(startPlus: 0)
+			let getdelim = File.Getdelim()
+			guard let type = try getdelim.read(from: self.file, delimitedBy: 0) else {
+				throw InvalidObject()
+			}
+			guard let arg = try getdelim.read(from: self.file, delimitedBy: 0) else {
+				throw InvalidObject()
+			}
+			return (type, arg)
+		}
 	}
 
-	func insert(file: File) throws -> H.Digest {
+	func lookup(hash: String) throws -> Object? {
+		let fd = try (dir/hash)._open(flags: Glibc.O_RDONLY, mode: 0)
+		let file = File(ptr: Glibc.fdopen(fd, "r"))
+		return try Object(hash: hash, file: file)
+	}
+
+	func insertBlob(file: File) throws -> String {
 		let f = try dir.makeTempFile()
 		guard ioctl_ficlone(f.fd, file.fd) != -1 else {
-			throw CError(errno: errno)
+			throw CError(errno: Glibc.errno)
 		}
 		var hasher = hf.init()
-		hasher.update(data: Array("blob\0".utf8))
+		hasher.update(sequence: "blob\0".utf8)
 		let hash = hasher.finalize()
-		try "user.fancfer.object-type".utf8CString.withUnsafeBufferPointer { (key_ptr) in
-			try Array("blob".utf8).withUnsafeBufferPointer { (val_ptr) in
-				guard fsetxattr(f.fd, key_ptr.baseAddress!, val_ptr.baseAddress!, val_ptr.count, XATTR_CREATE) != -1 else {
-					throw CError(errno: errno)
+		try "user.fancfer.object-type".withCString { (key_ptr) in
+			try "blob".utf8.withUnsafeBufferPointer { (val_ptr) in
+				guard FancferC.fsetxattr(f.fd, key_ptr, val_ptr.baseAddress!, val_ptr.count, XATTR_CREATE) != -1 else {
+					throw CError(errno: Glibc.errno)
 				}
 			}
 		}
-		try "/proc/self/fd/${f.fd}".utf8CString.withUnsafeBufferPointer { (src_ptr) in
-			try ref(hash).path.utf8CString.withUnsafeBufferPointer { (dst_ptr) in
-				guard linkat(AT_FDCWD, src_ptr.baseAddress!, dir.fd, dst_ptr.baseAddress!, AT_SYMLINK_FOLLOW) != -1 else {
-					throw CError(errno: errno)
+		try "/proc/self/fd/\(f.fd)".withCString { (src_ptr) in
+			try hash.hexString().withCString { (dst_ptr) in
+				if linkat(AT_FDCWD, src_ptr, dir.fd, dst_ptr, AT_SYMLINK_FOLLOW) == -1 {
+					let err = Glibc.errno
+					if err == EEXIST {
+					} else {
+						throw CError(errno: err)
+					}
 				}
 			}
 		}
-		return hash
+		return hash.hexString()
+	}
+
+	func build(type: String) throws -> BuildingObject<H> {
+		try BuildingObject(store: self, type: type)
+	}
+
+	class BuildingObject<H: HashFunction>: TextOutputStream {
+		let store: Store<H>
+		let type: String
+		var file: File
+		var hasher: H
+
+		init(store: Store<H>, type: String) throws {
+			self.store = store
+			self.type = type
+			self.file = try self.store.dir.makeTempFile()
+			self.hasher = store.hf.init()
+
+			try "user.fancfer.object-type".withCString { (key_ptr) in
+				try type.utf8.withUnsafeBufferPointer { (val_ptr) in
+					guard FancferC.fsetxattr(self.file.fd, key_ptr, val_ptr.baseAddress!, val_ptr.count, XATTR_CREATE) != -1 else {
+						throw CError(errno: Glibc.errno)
+					}
+				}
+			}
+		
+			self.hasher.update(sequence: self.type.utf8)
+			self.hasher.update(data: [0])
+		}
+
+		func write(_ string: String) {
+			self.file.write(string)
+			self.hasher.update(sequence: string.utf8)
+		}
+
+		func finalize() throws -> String {
+			let hash = self.hasher.finalize().hexString()
+			try "/proc/self/fd/\(self.file.fd)".withCString { (src_ptr) in
+				try hash.withCString { (dst_ptr) in
+					if linkat(AT_FDCWD, src_ptr, self.store.dir.fd, dst_ptr, AT_SYMLINK_FOLLOW) == -1 {
+						let err = Glibc.errno
+						if err == EEXIST {
+						} else {
+							throw CError(errno: err)
+						}
+					}
+				}
+			}
+			return hash
+		}
+	}
+
+	func insertBlob(str: String) throws -> String {
+		let builder = try build(type: "blob")
+		builder.write(str)
+		return try builder.finalize()
+	}
+
+	func insertDir(dict: Dictionary<String, String>) throws -> String {
+		let builder = try build(type: "dir")
+		for (name, hash) in dict {
+			builder.write(hash + "\0" + name + "\0")
+		}
+		return try builder.finalize()
+	}
+
+	func insertSrc(type: String, arg: String) throws -> String {
+		let builder = try build(type: "src")
+		builder.write(type + "\0")
+		builder.write(arg + "\0")
+		return try builder.finalize()
 	}
 }
 
 let root = try Directory(ref: FSRef(dir: nil, path: "../test-repo"))
 let s = Store(dir: try Directory(ref: root/".fancfer/store/sha256"), hf: SHA256.self)
-let hf = SHA256()
-let digest = hf.finalize()
-print(s.ref(digest))
+let buz = try s.insertBlob(str: "buz")
+print("buz", buz)
+let baz = try s.insertBlob(str: "baz")
+print("baz", baz)
+let dir = try s.insertDir(dict: ["fiz": buz, "bar": baz])
+print("dir", dir)
+
+try print(s.lookup(hash: dir)!.dir())
--- swift/Sources/FancferC/include/stuff.h
+++ swift/Sources/FancferC/include/stuff.h
@@ -1,4 +1,3 @@
-// #include<stdint.h>
 typedef long  int64_t;
 typedef int   int32_t;
 typedef short int16_t;
@@ -8,59 +7,8 @@ typedef unsigned short uint16_t;
 typedef unsigned long size_t;
 typedef long ssize_t;
 
-void free(void*);
-const char *strerror(int errnum);
-__thread extern int errno;
-#define ENOENT 2
-#define EEXIST 17
+#define O_TMPFILE 020200000
 
-#define AT_FDCWD -100
-
-typedef struct FILE FILE;
-FILE *fdopen(int fd, const char *mode);
-size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
-size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
-int fclose(FILE *stream);
-int64_t getdelim(char **lineptr, size_t *n, int delim, FILE *stream);
-int fileno(FILE *stream);
-enum fseek_whence {
-	SEEK_SET = 0,
-	SEEK_CUR = 1,
-	SEEK_END = 2
-};
-int fseek(FILE *stream, long offset, enum fseek_whence whence);
-long ftell(FILE *stream);
-
-#define O_RDONLY            00
-#define O_WRONLY            01
-#define O_RDWR              02
-#define O_CREAT           0100
-#define O_DIRECTORY    0200000
-#define O_PATH       010000000
-#define O_TMPFILE   (020000000 | O_DIRECTORY)
-
-#define S_IXOTH  0000001
-#define S_IWOTH  0000002
-#define S_IROTH  0000004
-#define S_IXGRP  0000010
-#define S_IWGRP  0000020
-#define S_IRGRP  0000040
-#define S_IXUSR  0000100
-#define S_IWUSR  0000200
-#define S_IRUSR  0000400
-#define S_ISVTX  0001000
-#define S_ISGID  0002000
-#define S_ISUID  0004000
-#define S_IFMT   0170000
-#define S_IFIFO  0010000
-#define S_IFCHR  0020000
-#define S_IFDIR  0040000
-#define S_IFBLK  0060000
-#define S_IFREG  0100000
-#define S_IFLNK  0120000
-#define S_IFSOCK 0140000
-int openat(int dirfd, const char *pathname, int flags, int mode);
-int close(int fd);
 struct statx_timestamp {
 	int64_t tv_sec;
 	uint32_t tv_nsec;
@@ -105,23 +53,13 @@ struct statx {
 	uint32_t stx_dev_minor;
 	uint64_t __spare2[14];
 };
-enum at_flags {
-	AT_SYMLINK_NOFOLLOW = 0x100,
-	AT_SYMLINK_FOLLOW = 0x400,
-	AT_EMPTY_PATH = 0x1000
-};
-int statx(int dirfd, const char *pathname, enum at_flags flags, enum statx_mask mask, struct statx *statxbuf);
-int64_t readlinkat(int dirfd, const char *pathname, char *buf, size_t bufsize);
-int symlinkat(const char *target, int newdirfd, const char *linkpath);
+int statx(int dirfd, const char *pathname, int32_t flags, enum statx_mask mask, struct statx *statxbuf);
 int ioctl_ficlone(int dest_fd, int src_fd);
 enum renameat2_flags {
 	RENAME_NOREPLACE = 1 << 0,
 	RENAME_EXCHANGE = 1 << 1
 };
 int renameat2(int olddirfd, const char *oldpath, int newdirfd, const char *newpath, enum renameat2_flags flags);
-int linkat(int olddirfd, const char *oldpath, int newdirfd, const char *newpath, enum at_flags flags);
-int unlinkat(int dirfd, const char *pathname, enum at_flags flags);
-int mkdirat(int dirfd, const char *pathname, int mode);
 
 int64_t llistxattr(const char *path, char *list, size_t size);
 int64_t flistxattr(int fd,           char *list, size_t size);
@@ -135,21 +73,5 @@ int lsetxattr(const char *path, const char *name, const void *value, size_t size
 int fsetxattr(int fd,           const char *name, const void *value, size_t size, enum setxattr_flags flags);
 int lremovexattr(const char *path, const char *name);
 int fremovexattr(int fd,           const char *name);
-
-struct dirent {
-	uint64_t d_ino;
-	int64_t d_off;
-	unsigned short int d_reclen;
-	unsigned char d_type;
-	char d_name[];
-};
-typedef struct DIR DIR;
-DIR *fdopendir(int fd);
-int closedir(DIR *dirp);
-struct dirent *readdir(DIR *dirp);
-int dirfd(DIR *dirp);
-void rewinddir(DIR *dirp);
-void seekdir(DIR *dirp, long loc);
-long telldir(DIR *dirp);
 
 char *get_current_dir_name(void);
